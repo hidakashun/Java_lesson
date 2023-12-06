4581f55aec8e3b0a1eb6d2acc2ff3f6704d7c492
hidakashun <hidakashun60@gmail.com>
2023-12-06 13:09:15 +0000

    Java練習、最も基本的なプログラム

---
+++ Sample101.java
@@ -0,0 +1,13 @@
+package day1; /*このプログラムはday1と呼ばれるパッケージに属することが示されます。*/
+
+public class Sample101 { /*クラス名*/
+    /*
+     * Javaを使った最も基本的なプログラム
+     */
+    public static void main(String[] args) { //  メインメソッドの宣言,処理の中身は、"{"および"}"で囲まれています。
+        System.out.println("Hello,World."); //  「Hello,World.」という文字列を表示します。
+    }
+
+}
+//  (String[] args)は、文字列型の配列で、プログラムが実行されるときにコマンドライン引数を受け取るために使用される
+//  System.out.println(内容);これは()で囲まれたものを、表示する命令
