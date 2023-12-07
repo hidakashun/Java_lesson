239b2eb7191468f78d6e605c7cc2fe229db778af
hidakashun <hidakashun60@gmail.com>
2023-12-07 14:24:57 +0000

    文字列と数値の表示

---
+++ progate/main.java
@@ -0,0 +1,7 @@
+class main {//  Javaでは、クラスの名前とファイル名は一致する必要があります。
+  public static void main(String[] args) {
+    // Javaでは文の終わりにセミコロン（;）を付けなければなりません。
+    System.out.println("Hello Java");
+    
+  }
+}
