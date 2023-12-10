e5cf2f6347ee8b91fc15fbaf234c4b279282d78d
hidakashun <hidakashun60@gmail.com>
2023-12-10 08:07:33 +0000

    変数の定義、代入

---
+++ progate/variable.java
@@ -0,0 +1,28 @@
+class variable {
+  public static void main(String[] args) {
+    // int型の変数numberを定義してください
+    int number;
+    
+    // 変数numberに3を代入してください
+    number = 3;
+    
+    // 変数numberを出力してください
+    System.out.println(number);
+
+    // String型の変数nameを定義してください
+    String name;
+    
+    // 変数nameに"Wanko"を代入してください
+    name = "Wanko";
+    
+    // 変数nameを出力してください
+    System.out.println(name);
+    
+  }
+}
+
+//intは整数(integer)、int型のiは小文字
+
+//Stringは文字の並び、String型のSは大文字
+
+//「=」はプログラミングにおいて、右の値を左のものに代入するという意味です。
