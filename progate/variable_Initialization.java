20b40bc98b39b7681cba6159b1ecbcda4b1cf436
hidakashun <hidakashun60@gmail.com>
2023-12-11 14:07:17 +0000

    変数の初期化と連結

---
+++ progate/variable_Initialization.java
@@ -0,0 +1,21 @@
+class variable_Initialization {
+  public static void main(String[] args) {
+    //変数の値の代入は変数定義と同時に行うことができます。変数定義と同時に値を代入することを変数の初期化と呼びます。
+    //数値が入った変数なら、数値と同様に計算が可能です。数値と変数の計算も、変数同士の計算もできます。
+    int number1 = 3;
+    
+    // int型の変数number2を定義し、7を代入してください
+    int number2 = 7;
+    
+    // number1 * number2を出力してください
+    System.out.println(number1 * number2);
+    
+    // 変数textに「プログラミングを勉強しよう」を代入してください
+    String text = "プログラミングを勉強しよう";
+    
+    // 「Progateで」と変数textを連結して出力してください
+    //変数には""をつけないように注意しましょう！
+    System.out.println("Progateで" + text);
+      
+  }
+}
