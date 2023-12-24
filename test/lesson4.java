cbfd139e1164423a1a7c9dd8910bccd5c70d0ae9
hidakashun <hidakashun60@gmail.com>
2023-12-24 11:35:50 +0000

    2つの整数変数 a と b を宣言し、それぞれの値を足して結果を表示するプログラムを作成してください。

---
+++ test/lesson4.java
@@ -0,0 +1,17 @@
+package test;
+
+public class lesson4 {
+
+	public static void main(String[] args) {
+		// 2つの整数変数 a と b を宣言し、それぞれの値を足して結果を表示するプログラムを作成してください。
+		
+		int a = 9;
+		int b = 5;
+		
+		int c = a + b;
+		
+		System.out.println("aとbを足した結果は"+c);
+		
+	}
+
+}
