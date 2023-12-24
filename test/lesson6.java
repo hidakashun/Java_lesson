6230007fc57b78deeae8c3635808264edec1505a
hidakashun <hidakashun60@gmail.com>
2023-12-24 12:04:29 +0000

    繰り返し文

---
+++ test/lesson6.java
@@ -0,0 +1,12 @@
+package test;
+
+public class lesson6 {
+
+	public static void main(String[] args) {
+		// 1から10までの整数を繰り返し文を使用して表示するプログラムを作成してください。
+		for (int num = 1; num <= 10; num++) {
+			System.out.println(num);
+		}
+	}
+
+}
