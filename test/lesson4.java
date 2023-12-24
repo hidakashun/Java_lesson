24f4a539264e9a76e84ab9f2a51fd4b738f1e6f9
hidakashun <hidakashun60@gmail.com>
2023-12-24 11:59:28 +0000

    変数

--- test/lesson4.java
+++ test/lesson4.java
@@ -3,7 +3,7 @@ package test;
 public class lesson4 {
 
 	public static void main(String[] args) {
-		// 2つの整数変数 a と b を宣言し、それぞれの値を足して結果を表示するプログラムを作成してください。
+		// 2つの整数変数 a と b を宣言し、それぞれの値を足して結果を表示するプログラムを作成してください.
 		
 		int a = 9;
 		int b = 5;
