6a387cf08ff14d90dbe1ca1a9d8f4bbdec25b128
hidakashun <hidakashun60@gmail.com>
2024-01-03 12:19:21 +0000

    1月2日練習

--- test/Q1_1.java
+++ test/Q1_1.java
@@ -1,15 +1,15 @@
 package test;
 
-public class Q11 {
+public class Q1_1 {
 
 	public static void main(String[] args) {
 		// 変数: 2つの整数変数を宣言し、それらの合計を表示するプログラムを書いてください。
-		
+
 		int num1 = 5;
 		int num2 = 3;
-		
+
 		int sum = num1 + num2;
-		
+
 		System.out.println(sum);
 
 	}
